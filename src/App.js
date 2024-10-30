e192dfa9dedd400342a65dadb4e26c209923d330
lmoraesdev <lbc_moraes@outlook.com>
2024-10-30 12:33:21 -0300

    feat: components pesquisa

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import Header from './components/header';
 import styled from 'styled-components';
+import Pesquisa from './components/pesquisa';
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
-
-  li {
-    list-style: none;
-  }
 `;
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
